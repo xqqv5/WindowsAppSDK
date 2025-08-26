243d6157dd3bf85deb01d34711151f2e3440540e
Haonan Tang (from Dev Box) <haonantang@microsoft.com>
2025-08-26 11:30:48 +0800

    update

---
+++ test/WindowsAppSDK.Test.NetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WindowsAppSDK.Test.NetCore
+{
+    // This app justs exists as a convenient way to output the .net 5 runtime binaries to BuildOutput
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
